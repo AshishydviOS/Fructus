64a46772c75217159307a98934b0f9f2183527fd
AshishydviOS <74372201+AshishydviOS@users.noreply.github.com>
2022-04-22 00:04:50 +0530

    Project created in Xcode 13.2.1

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ashish Yadav on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
